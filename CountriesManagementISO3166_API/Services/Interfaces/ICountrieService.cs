8b88a0c2df1dcbe32284f395024f252e46d79c7b
Gustavo <gsantiestebanp@gmail.com>
2021-07-11 16:07:51 +0200

    [ADD] Creación de servicios

---
+++ CountriesManagementISO3166_API/Services/Interfaces/ICountrieService.cs
@@ -0,0 +1,16 @@
+﻿using Countries_Management_ISO3166_API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Countries_Management_ISO3166_API.Services
+{
+    public interface ICountrieService
+    {
+        Task<List<Countrie>> GetAllCountries();
+        Task<Countrie> GetCountrieByCommonName(string commonName);
+        Task<Countrie> GetCountrieByAlpha2Code(string alpha2Code);
+        Task InsertCountrie(Countrie countrie);
+        Task UpdateCountrie(Countrie countrie);
+        Task DeleteCountrie(Countrie countrie);
+    }
+}
---
+++ CountriesManagementISO3166_API/Services/Interfaces/ISubdivisionService.cs
@@ -0,0 +1,15 @@
+﻿using Countries_Management_ISO3166_API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Countries_Management_ISO3166_API.Services.Interfaces
+{
+    public interface ISubdivisionService
+    {
+        Task<List<Subdivision>> GetAllSubdivisions();
+        Task<Subdivision> GetSubdivisionById(int id);
+        Task InsertSubdivision(Subdivision subdivision);
+        Task UpdateSubdivision(Subdivision subdivision);
+        Task DeleteSubdivision(Subdivision subdivision);
+    }
+}
