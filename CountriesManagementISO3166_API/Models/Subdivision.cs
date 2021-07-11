33cf8139fa4caaadcc57f617221411da5e5e6e65
Gustavo <gsantiestebanp@gmail.com>
2021-07-11 16:05:40 +0200

    [ADD] Creación de modelos de datos

---
+++ CountriesManagementISO3166_API/Models/Subdivision.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Countries_Management_ISO3166_API.Models
+{
+    public class Subdivision
+    {
+        [Key]
+        [Required]
+        public int SubdivisionId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string SubdivisionCode { get; set; }
+    }
+}
