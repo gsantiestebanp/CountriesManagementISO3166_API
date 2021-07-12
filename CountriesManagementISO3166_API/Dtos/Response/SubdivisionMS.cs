6fd426fe21ac0f88056f60ce0b77e0a6ee29f738
Gustavo <gsantiestebanp@gmail.com>
2021-07-12 16:00:18 +0200

    [FIX] Correcciones en los modelos y dtos

--- CountriesManagementISO3166_API/Dtos/Response/SubdivisionMS.cs
+++ CountriesManagementISO3166_API/Dtos/Response/SubdivisionMS.cs
@@ -4,13 +4,8 @@ namespace CountriesManagementISO3166_API.Dtos
 {
     public class SubdivisionMS
     {
-        [Required]
         public int SubdivisionId { get; set; }
-
-        [Required]
         public string Name { get; set; }
-
-        [Required]
         public string SubdivisionCode { get; set; }
     }
 }
